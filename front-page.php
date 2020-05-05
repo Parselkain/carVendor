e5cbf106cc880ca4619ce1cdd6e1e06c72e36e6a
Parselkain <beltramidesign@gmail.com>
2020-05-05 15:54:29 +0200

    start develop front-page

--- front-page.php
+++ front-page.php
@@ -1,5 +1,14 @@
 <?php get_header(); ?>
 <div class="main-content">
+    <div class="container">
+        <section>
+            <div class="row">
+                <div class="asdasda"></div>
+            </div>
+        
+        </section>
+    
+    </div>
 
 </div>
 
